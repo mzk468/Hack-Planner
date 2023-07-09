395e068f0e44c57d26cb3cc17ea91644abab008d
mzk468 <mzkhaled468@gmail.com>
2023-07-09 17:21:34 +0100

    Delete functionality added

--- app.py
+++ app.py
@@ -22,3 +22,9 @@ def edit_item(item_id):
     return redirect('/')
   
   return render_template('edit.html', item=item, item_id=item_id)
+
+# Delete functionality
+@app.route('/delete/<int:item_id>')
+def delete_item(item_id):
+  del items[item_id - 1]
+  return redirect('/')
