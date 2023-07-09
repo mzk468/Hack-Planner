cdf1d44ffb0b6aca26ed2c1a1f060e1ebfdf76ea
mzk468 <mzkhaled468@gmail.com>
2023-07-09 17:10:50 +0100

    View function for adding item to `items` list

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+items = [] # No DB for now, simply using list
+
+@app.route('/add', method=['POST'])
+def add_item():
+  item = request.form['item']
+  items.append(item)
+  return redirect('/') # Go back to old page
