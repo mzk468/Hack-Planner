e162f11e7a266c941c59d4add73acbac7d63ab06
mzk468 <mzkhaled468@gmail.com>
2023-07-28 11:23:55 +0100

    Refactoring db functions, all now contain db_ prefix

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ def create_table():
   connection.commit()
   connection.close()
 
-def add_item(item):
+def db_add_item(item):
   connection = sqlite3.connect(db_path)
   cursor = connection.cursor()
   cursor.execute("INSERT INTO checklist (item) VALUES (?)", (item,))
   connection.commit()
   connection.close()
 
-def get_items():
+def db_get_items():
   connection = sqlite3.connect(db_path)
   cursor = connection.cursor()
   cursor.execute("SELECT * FROM checklist")
@@ -30,14 +30,14 @@ def get_items():
   connection.close()
   return items
 
-def update_item(item_id, new_item):
+def db_update_item(item_id, new_item):
   connection = sqlite3.connect(db_path)
   cursor = connection.cursor()
   cursor.execute("UPDATE checklist SET item = ? WHERE id = ?", (new_item, item_id))
   connection.commit()
   connection.close()
 
-def delete_item(item_id):
+def db_delete_item(item_id):
   connection = sqlite3.connect(db_path)
   cursor = connection.cursor()
   cursor.execute("DELETE FROM checklist WHERE id = ?", (item_id,))
